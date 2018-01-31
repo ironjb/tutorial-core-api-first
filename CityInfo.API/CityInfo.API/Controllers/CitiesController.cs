05df823afaa248145240a9472a03b265a386a4da
ironjb <ironjb20@gmail.com>
2018-01-31 14:31:19 -0700

    attribute routing and return static data

---
+++ CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+	[Route("api/cities")]		// <-- could be "api/[controller]" (not recommended by course instructor)
+	public class CitiesController : Controller
+	{
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+		//[HttpGet("api/cities")]		// <-- if not using Route attribute at controller level
+		[HttpGet()]
+		public JsonResult GetCities()
+		{
+			return new JsonResult(new List<object>() {
+				new { id = 1, Name = "Boise" }
+				, new { id = 2, Name = "Nampa" }
+			});
+		}
+	}
+}
