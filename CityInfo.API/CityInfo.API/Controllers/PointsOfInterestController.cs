9156de01d468d43645b2b5f4c8836d2fa60fb305
ironjb <ironjb20@gmail.com>
2018-02-02 14:45:20 -0700

    custom service with dependency injection (concrete type)

--- CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,9 +14,11 @@ namespace CityInfo.API.Controllers
 	public class PointsOfInterestController : Controller
 	{
 		private readonly ILogger<PointsOfInterestController> _logger;
+		private readonly LocalMailService _mailService;
 
-		public PointsOfInterestController(ILogger<PointsOfInterestController> logger) {
+		public PointsOfInterestController(ILogger<PointsOfInterestController> logger, LocalMailService mailService) {
 			_logger = logger;
+			_mailService = mailService;
 		}
 
 		[HttpGet("{cityId}/pointsofinterest")]
@@ -176,6 +179,8 @@ namespace CityInfo.API.Controllers
 			}
 
 			city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+			_mailService.Send("Point of Interest deleted", $"Point of interest {pointOfInterestFromStore.Name} with id {pointOfInterestFromStore.Id} was deleted.");
 
 			return NoContent();
 		}
--- CityInfo.API/CityInfo.API/Startup.cs
+++ CityInfo.API/CityInfo.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,8 @@ namespace CityInfo.API
 					castedResolver.NamingStrategy = null;
 				}
 			}); */
+
+			services.AddTransient<LocalMailService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
